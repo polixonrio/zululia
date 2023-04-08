c322dbbc3e7d4cdf0d118bda369182eb82dc0e1d
polixonrio <polixonrio@gmail.com>
2023-04-08 11:42:56 +0000

    lmao

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,7 @@
 					style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 				/>
 			</div>
-			<div class="mx-auto max-w-2xl py-32 md:py-48">
+			<div class="mx-auto max-w-2xl py-48 md:py-22">
 				<div class="hidden sm:mb-8 sm:flex sm:justify-center">
 					<div
 						class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray dark:text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20"
