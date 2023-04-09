ddfb92f074bbe41295af3aa28427ddd3bd8aa90d
polixonrio <polixonrio@gmail.com>
2023-04-09 10:54:23 +0000

    lmao

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -49,11 +49,11 @@
 	// 		activateClickOutside = true;
 	// 	}
 	// });
-	let darkmodebtn =
-		'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5  right-2 top-12  md:top-3 md:right-2 z-50';
-	let divClass = 'w-full md:block md:w-auto';
-	let ulClass =
-		'flex flex-col p-4 text-center mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium';
+	// let darkmodebtn =
+	// 	'text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-lg p-2.5  right-2 top-12  md:top-3 md:right-2 z-50';
+	// let divClass = 'w-full md:block md:w-auto';
+	// let ulClass =
+	// 	'flex flex-col p-4 text-center mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium';
 </script>
 
 <!-- <svelte:window bind:innerWidth={width} /> -->
