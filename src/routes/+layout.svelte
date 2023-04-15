7caad7f098d695a66cd20eba8bbfdec327b50a23
polixonrio <polixonrio@gmail.com>
2023-04-15 07:26:56 +0000

    fotest2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -64,9 +64,11 @@
 		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 		<a href="https://flowbite.com/" class="flex items-center">
 			<img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
-			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+			<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Asterisc</span>
 		</a>
 		<div class="flex md:order-2">
+			<DarkMode />
+
 			<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
 			<button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
 			  <span class="sr-only">Open main menu</span>
