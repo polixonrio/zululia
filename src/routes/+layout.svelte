9ff6b67d548f9a4a50c606fd4c73d6660a9809e7
polixonrio <polixonrio@gmail.com>
2023-04-15 08:43:14 +0000

    fotest5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -79,7 +79,7 @@
 			  <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
 			</button>
 		</div>
-		<div class=" {isOpen ? '' : hidden} items-center justify-between  w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+		<div class=" {isOpen ? '' : 'hidden'} items-center justify-between  w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
 		  <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 			<li>
 			  <a href="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
