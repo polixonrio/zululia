c4e95686a58f14a201f4fc90d566b00d4994edf9
polixonrio <polixonrio@gmail.com>
2023-04-15 10:18:44 +0000

    fotest10

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -64,7 +64,7 @@
 
 <header class="sticky shadow-xl dark:shadow-indigo-500/50 top-0 z-50 ">
 
-	<!-- <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"> -->
+	<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
 		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 		<a href="/" class="flex items-center">
 			<img src="/images/logos.webp" class="h-8 mr-3" alt="Asterisc Logo">
